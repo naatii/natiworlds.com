ebe385a5a9b66e869501306318aa7797a9d48f36
naatii <jcorber855@g.educaand.es>
2023-01-30 23:31:56 +0100

    tarea extra terminada

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function validar(){
+    validarNumero()
+    validarEmail()
+    validarEdad()
+    if (validarNumero() && validarEdad() && validarEdad()){
+        document.location.href = "/../index.html"
+    }
+}
+function validarNumero(){
+    let numero = document.getElementById("movil").value
+    if (numero.match("(^6|^7)[0-9]{8}")){
+        console.log (numero+" correcto")   
+        return true     
+    }
+    else{
+        alert("Entrada inválida")
+    }
+    return false
+}
+function validarEmail(){
+    let email = document.getElementById("email").value
+    if (email.match( /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ )){
+        console.log(email+" correcto")
+        return true
+    }
+    else{
+        alert("Entrada inválida")
+    }
+    return false
+}
+function validarEdad(){
+    let edad = document.getElementById("edad").value
+    parseInt(edad)
+    if(edad>=99){
+        alert("Edad inválida")
+        return false
+    }
+    if(edad<=0){
+        alert("Edad inválida")
+        return false
+    }
+    else{
+        console.log(edad+" correcta")
+        return true
+    }
+}
